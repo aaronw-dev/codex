7cbd091d86a5be97c03d12506dc32d90dcde9257
AW_Dev <riptidegamesceo@gmail.com>
2024-07-14 11:21:45 -0700

    hope this works!

--- api/static/js/app.js
+++ api/static/js/app.js
@@ -1,7 +1,12 @@
 if (localStorage.getItem("setup") == null) {
     location.href = "/onboarding"
 }
-
-fetch(`/courses/${localStorage.getItem("selected_lang")}/1`)
-    .then(data => data.json())
-    .then(json => console.log(json))
+const courseTitle = document.getElementById("course-title")
+var courseInfo;
+async function init() {
+    await fetch(`/courses/${localStorage.getItem("selected_lang")}/1`)
+        .then(data => data.json())
+        .then(json => courseInfo = json)
+    console.lot(courseInfo)
+}
+init()
