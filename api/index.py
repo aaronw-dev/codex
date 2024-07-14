32326176c9b42fde0354c8199410aa1ea6d57429
AW_Dev <riptidegamesceo@gmail.com>
2024-07-13 17:28:39 -0700

    asdfasfdafs

--- api/index.py
+++ api/index.py
@@ -4,23 +4,12 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import json
 import os
-
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-creds_dict = {
-    "type": "service_account",
-    "project_id": os.environ['project_id'],
-    "private_key_id": os.environ['private_key_id'],
-    "private_key": os.environ['private_key'],
-    "client_email": os.environ['client_email'],
-    "client_id": os.environ['client_id'],
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": os.environ['client_x509_cert_url'],
-    "universe_domain": "googleapis.com"
-}
+creds_dict = json.loads(os.environ.get(
+    ("FIREBASE_SERVICE_ACCOUNT_CREDENTIAL")))
 cred = credentials.Certificate(creds_dict)
+
 app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
