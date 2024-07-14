dc78e8afe5941bcb2152ee2f90852354acaac9ee
AW_Dev <riptidegamesceo@gmail.com>
2024-07-14 11:14:12 -0700

    test

--- api/static/js/app.js
+++ api/static/js/app.js
@@ -1,3 +1,7 @@
 if (localStorage.getItem("setup") == null) {
     location.href = "/onboarding"
 }
+
+fetch(`/courses/${localStorage.getItem("selected_lang")}/1`)
+    .then(data => data.json())
+    .then(json => console.log(json))
